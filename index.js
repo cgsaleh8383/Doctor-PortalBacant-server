1086eeac0cae3d5a1838e802ee8db28e4177c076
md sajjat hossain <cgsaleh83@gmail.com>
2021-01-06 16:05:31 +0600

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(fileUpload())
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send("hello from db it's working working")
+    res.send("hello from db it's working")
 })
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
